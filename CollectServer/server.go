705574e569a41f4d83af3101dcb1f91d227e4f1b
kukaro <justkukaro@naver.com>
2018-10-01 21:23:41 +0900

    cors

--- CollectServer/server.go
+++ CollectServer/server.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"fmt"
-	"./conf"
-	"./router"
+	"github.com/googollee/go-socket.io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	if err := conf.Init(""); err == nil {
-		fmt.Println("config success")
+	//if err := conf.Init(""); err == nil {
+	//	fmt.Println("config success")
+	//}
+	//router.RunSubDomains()
+
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-	router.RunSubDomains()
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		fmt.Println("on connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			log.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+	http.HandleFunc("/socket.io/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		server.ServeHTTP(w, r)
+	})
+	//http.Handle("/socket.io/", server)
+	//http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:3100...")
+	log.Fatal(http.ListenAndServe(":3100", nil))
 }
