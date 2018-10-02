dc66b1124933cdcc9183172447cd02bcd3ac960f
kukaro <justkukaro@naver.com>
2018-10-03 00:40:22 +0900

    adf

---
+++ init.sh
@@ -0,0 +1,5 @@
+go get -u github.com/labstack/echo/...
+go get -u github.com/go-sql-driver/mysql
+go get -u golang.org/x/net/websocket
+go get -u github.com/googollee/go-socket.io
+go get -u github.com/rs/cors
