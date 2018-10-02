7a0bce89ce0ad91aad4077195ae1d575d397e422
kukaro <justkukaro@naver.com>
2018-10-03 00:54:02 +0900

    asd

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 rm -rf ~/go
 
+export GOPATH=/home/ubuntu/go
+
 go get -u github.com/labstack/echo/...
 go get -u github.com/go-sql-driver/mysql
 go get -u golang.org/x/net/websocket
 go get -u github.com/googollee/go-socket.io
 go get -u github.com/rs/cors
+
+go run gCollectServer/server.go
