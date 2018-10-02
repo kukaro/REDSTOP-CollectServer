80f764417eb57dea93602c65cce2b87e42ce7e21
kukaro <justkukaro@naver.com>
2018-10-02 22:19:01 +0900

    test

--- CollectServer/server.go
+++ CollectServer/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"./conf"
+	"./router"
 	"fmt"
 	"github.com/googollee/go-socket.io"
 	"log"
@@ -8,38 +10,43 @@ import (
 )
 
 func main() {
-	//if err := conf.Init(""); err == nil {
-	//	fmt.Println("config success")
-	//}
-	//router.RunSubDomains()
+	go func() {
+		server, err := socketio.NewServer(nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		server.On("connection", func(so socketio.Socket) {
+			log.Println("on connection")
+			fmt.Println("on connection")
+			//so.Join("chat")
+			//so.On("chat message", func(msg string) {
+			//	log.Println("emit:", so.Emit("chat message", msg))
+			//	so.BroadcastTo("chat", "chat message", msg)
+			//})
+			so.On("disconnection", func() {
+				log.Println("on disconnect")
+			})
+			so.On("reqSignIn", func(data map[string]string) {
+				log.Println(data["id"])
+			})
+		})
+		server.On("error", func(so socketio.Socket, err error) {
+			log.Println("error:", err)
+		})
+		http.HandleFunc("/socket.io/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			server.ServeHTTP(w, r)
+		})
+		//http.Handle("/socket.io/", server)
+		//http.Handle("/", http.FileServer(http.Dir("./asset")))
+		log.Println("Serving at localhost:3100...")
+		log.Fatal(http.ListenAndServe(":3100", nil))
+	}()
 
-
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
+	if err := conf.Init(""); err == nil {
+		fmt.Println("config success")
 	}
-	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
-		fmt.Println("on connection")
-		so.Join("chat")
-		so.On("chat message", func(msg string) {
-			log.Println("emit:", so.Emit("chat message", msg))
-			so.BroadcastTo("chat", "chat message", msg)
-		})
-		so.On("disconnection", func() {
-			log.Println("on disconnect")
-		})
-	})
-	server.On("error", func(so socketio.Socket, err error) {
-		log.Println("error:", err)
-	})
-	http.HandleFunc("/socket.io/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		server.ServeHTTP(w, r)
-	})
-	//http.Handle("/socket.io/", server)
-	//http.Handle("/", http.FileServer(http.Dir("./asset")))
-	log.Println("Serving at localhost:3100...")
-	log.Fatal(http.ListenAndServe(":3100", nil))
+	router.RunSubDomains()
+
 }
--- README.md
+++ README.md
@@ -4,4 +4,22 @@
 ```
 go get -u github.com/labstack/echo/...
 go get -u github.com/go-sql-driver/mysql
+go get -u golang.org/x/net/websocket
+go get -u github.com/googollee/go-socket.io
+go get -u github.com/rs/cors
+```
+
+## 사용한 예제
+```
+https://github.com/hb-go/echo-web/blob/master/router/socket/socket.go
+https://alligator.io/vuejs/vue-socketio/
+웹소켓 사용 예제
+
+https://github.com/googollee/go-socket.io/issues/21
+socketio이슈 해결하는 방법
+```
+
+##Test
+```
+test
 ```
